c2314cd4a640bb59a34e50cc445b778541c638f7
BizonNT <137486029+BizonNT@users.noreply.github.com>
2024-01-21 17:52:54 +0200

    App changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
@@ -6,69 +6,57 @@ import Notification from './Notification/Notification';
 
 import css from './app.module.css';
 
-class App extends Component {
-  static voteOptions = ['good', 'neutral', 'bad'];
+export default function App() {
+  const voteOptions = ['good', 'neutral', 'bad'];
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  makeFirstLetterToUpperCase = value => {
+  const makeFirstLetterToUpperCase = value => {
     return value.toUpperCase().slice(0, 1) + value.slice(1);
   };
 
-  addVote = keyName => {
-    this.setState(prevState => {
-      return {
-        [keyName]: prevState[keyName] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    if (!total) {
-      return 0;
+  const addVote = keyName => {
+    switch (keyName) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
     }
-    const { good } = this.state;
-    return Number(((good / total) * 100).toFixed(0));
-  }
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalVotes = this.countTotalFeedback();
-    return (
-      <div className={css.container}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={App.voteOptions}
-            addVote={this.addVote}
-            makeFirstLetterToUpperCase={this.makeFirstLetterToUpperCase}
+  const totalVotes = good + neutral + bad;
+
+  return (
+    <div className={css.container}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={voteOptions}
+          addVote={addVote}
+          makeFirstLetterToUpperCase={makeFirstLetterToUpperCase}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalVotes > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalVotes}
+            positivePercentage={Number(((good / totalVotes) * 100).toFixed(0))}
           />
-        </Section>
-        <Section title="Statistics">
-          {totalVotes > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalVotes}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
 }
-
-export default App;
